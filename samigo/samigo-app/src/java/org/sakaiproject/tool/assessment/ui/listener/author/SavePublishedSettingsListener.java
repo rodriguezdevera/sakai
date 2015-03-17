0868409bff762bbc6423e2b7f37068d12ce39dbe
unknown <jrodriguez@um.es>
2015-03-17 18:25:32 +0100

    SAM-2512 Checks if samigo.autoSubmit.enabled property is enabled before displaying the error message

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/author/SavePublishedSettingsListener.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/author/SavePublishedSettingsListener.java
@@ -39,6 +39,7 @@ import javax.faces.event.ActionListener;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import org.sakaiproject.component.cover.ServerConfigurationService;
 import org.sakaiproject.event.cover.EventTrackingService;
 import org.sakaiproject.service.gradebook.shared.GradebookExternalAssessmentService;
 import org.sakaiproject.service.gradebook.shared.GradebookService;
@@ -290,9 +291,12 @@ implements ActionListener
 	    }
 	    // if auto-submit is enabled, make sure late submission date is set
 	    if (assessmentSettings.getAutoSubmit() && retractDate == null) {
-	    	String dateError4 = ContextUtil.getLocalizedString("org.sakaiproject.tool.assessment.bundle.AssessmentSettingsMessages","retract_required_with_auto_submit");
-	    	context.addMessage(null,new FacesMessage(FacesMessage.SEVERITY_WARN, dateError4, null));
-	    	error=true;
+	    	String autoSubmitEnabled = ServerConfigurationService.getString("samigo.autoSubmit.enabled");
+	  	  	if ("true".equalsIgnoreCase(autoSubmitEnabled)) {
+	  	  		String dateError4 = ContextUtil.getLocalizedString("org.sakaiproject.tool.assessment.bundle.AssessmentSettingsMessages","retract_required_with_auto_submit");
+	  	  		context.addMessage(null,new FacesMessage(FacesMessage.SEVERITY_WARN, dateError4, null));
+	  	  		error=true;
+	  	  	}
 	    }
 	    	    
 		// if timed assessment, does it has value for time
